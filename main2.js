5b30afd3b9753c02a1dac0d3d63894d72d642d29
AntoineKoyuki <63412402+AntoineKoyuki@users.noreply.github.com>
2020-04-19 16:16:54 +0200

    Update main2.js
    avancée

--- main2.js
+++ main2.js
@@ -1,4 +1,3 @@
-//class guerrier
 class Guerrier {
 	constructor(_name, _xp, _type, _level){
 		this.name = _name
@@ -12,6 +11,26 @@ class Guerrier {
 	}
 	}
 
-let fighter1 = new Guerrier("Ben", 0, "Guerrier", 1)
+class Gladiator {
+	constructor(_name, _xp, _type, _level){
+		this.name = _name
+		this.xp = _xp
+		this.type = _type
+		this.level = _level
+	}
+}
 
+class Arena{
+	constructor(){
+
+	}
+	fight (fighter1, fighter2){
+		console.log("Les couillus " + fighter1.name + " et " + fighter2.name + " vont se castagner !")
+	}
+} 
+
+let fighter1 = new Guerrier("Ben", 0, "Guerrier", 1)
+let fighter2 = new Gladiator("Jack", 0, "Gladiator", 1)
 console.log(fighter1.name + " arrive !")
+let arena = new Arena()
+arena.fight(fighter1, fighter2)
